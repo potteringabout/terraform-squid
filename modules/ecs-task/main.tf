2ccf6c16d2199a86e2d81d5b504f4a8c45e2ab9a
Antony Potter <tone@potteringabout.net>
2023-11-21 23:11:45 +0000

    CPU and Memory

--- modules/ecs-task/main.tf
+++ modules/ecs-task/main.tf
@@ -4,6 +4,8 @@ resource "aws_ecs_task_definition" "task" {
   network_mode             = "awsvpc"
   task_role_arn            = var.task_role_arn
   execution_role_arn       = var.execution_role_arn
+  cpu                      = 1024
+  memory                   = 2048
 
   container_definitions = jsonencode(var.container_definitions)
 
