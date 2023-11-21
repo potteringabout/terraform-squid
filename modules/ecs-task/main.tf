1d117c907586cd9724d5633d82b9dfdd3ceac03b
Antony Potter <tone@potteringabout.net>
2023-11-21 23:06:58 +0000

    Added VPC setting for Fargate

--- modules/ecs-task/main.tf
+++ modules/ecs-task/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecs_task_definition" "task" {
   family                   = var.task_family
   requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   task_role_arn            = var.task_role_arn
   execution_role_arn       = var.execution_role_arn
 
