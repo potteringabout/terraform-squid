f177ad975cdc6beb350a36495c5d6bc5c3dc71ae
Antony Potter <tone@potteringabout.net>
2024-02-25 19:27:14 +0000

    Added vpc

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -2,53 +2,52 @@ locals {
   vpc_cidr = "10.0.0.0/22"
   region = "eu-west-1"
 
-  subs = {
-    access = [
-      {
-        cidr = "10.0.0.0/25"
-        availability_zone = "${local.region}a"
-      },
-      {
-        cidr = "10.0.1.128/25"
-        availability_zone = "${local.region}b"
-      },
-    ]
-    app = [
-      {
-        cidr = "10.0.1.0/25"
-        availability_zone = "${local.region}a"
-      },
-      {
-        cidr = "10.0.1.128/25"
-        availability_zone = "${local.region}b"
-      },
-    ],
-    data = [
-      {
-        cidr = "10.0.2.0/25"
-        availability_zone = "${local.region}a"
-      },
-      {
-        cidr = "10.0.2.128/25"
-        availability_zone = "${local.region}b"
-      },
-    ]
-  }
+  access_subnets = [
+    {
+      cidr = "10.0.0.0/25"
+      availability_zone = "${local.region}a"
+    },
+    {
+      cidr = "10.0.1.128/25"
+      availability_zone = "${local.region}b"
+    },
+  ]
+  app_subnets = [
+    {
+      cidr = "10.0.1.0/25"
+      availability_zone = "${local.region}a"
+    },
+    {
+      cidr = "10.0.1.128/25"
+      availability_zone = "${local.region}b"
+    },
+  ],
+  data_subnets = [
+    {
+      cidr = "10.0.2.0/25"
+      availability_zone = "${local.region}a"
+    },
+    {
+      cidr = "10.0.2.128/25"
+      availability_zone = "${local.region}b"
+    },
+  ]
 
-  subnets = flatten([
+
+  /*subnets = flatten([
     for subnet_type, subnet_defs  in local.subs: 
       [
         for idx, subnet_def in subnet_defs: merge(subnet_def, {type = subnet_type, name = "${subnet_type}${idx+1}"})
       ]
-  ])
+  ])*/
   
 }
 
 resource "aws_subnet" "subnets" {
-  for_each = toset(local.subnets)
+  for_each = tomap(local.access_subnets)
 
   vpc_id     = aws_vpc.main.id
-  availability_zone = each.value["availability_zone"]
+  availability_zone = each.value.availability_zone
   
   
   # each.value here is a value from var.vpcs
