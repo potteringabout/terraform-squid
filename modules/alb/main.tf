345ff44e842239a3cdd5aab76850ba192cb4cf2f
Antony Potter <tone@potteringabout.net>
2024-03-20 23:18:20 +0000

    ALB check

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,9 +51,9 @@ resource "aws_lb_target_group" "target_group" {
   target_type = "ip"
   vpc_id      = var.vpc_id
   health_check {
-    path                = "/health"
+    path                = "/"
     protocol            = "HTTP"
-    matcher             = "200"
+    matcher             = "200-499"
     port                = "traffic-port"
     healthy_threshold   = 2
     unhealthy_threshold = 2
